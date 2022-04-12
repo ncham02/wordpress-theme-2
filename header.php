325ff6405633c1983140fe49c50688360e3f4aa0
nia-hamilton <nia.hamilton@stageslearning.com>
2022-04-11 22:09:52 -0400

    Edited header.php file

--- header.php
+++ header.php
@@ -19,9 +19,3 @@
     </section>
 
     <section class="main">
-      <section class="graphics">
-        <div class="graphic cyan"></div>
-        <div class="graphic green"></div>
-        <div class="graphic yellow"></div>
-        <div class="graphic red"></div>
-      </section>
