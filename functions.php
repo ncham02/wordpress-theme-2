13c7ec0ad164a8dd22c0896e12e47f4d7798485e
nia-hamilton <nia.hamilton@stageslearning.com>
2022-04-15 16:36:35 -0400

    Changed template names to beautiful-theme-top-menu

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ add_theme_support('menus');
 add_theme_support('post-thumbnails');
 
 function wpb_custom_new_menu() {
-  register_nav_menu('my-custom-menu',__( 'My Custom Menu' ));
+  register_nav_menu('beautiful-theme-top-menu',__( 'Beautiful Theme Top Menu' ));
 }
 add_action( 'init', 'wpb_custom_new_menu' );
 
