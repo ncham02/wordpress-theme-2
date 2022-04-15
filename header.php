50fbeb37e422136b9c37b58412abbbc35030ac73
nia-hamilton <nia.hamilton@stageslearning.com>
2022-04-15 16:41:47 -0400

    Updated header menu name to beaut_theme_top_menu

--- header.php
+++ header.php
@@ -11,8 +11,8 @@
             <nav>
     <?php wp_nav_menu(
         array(
-          'theme_location' => 'top_menu',
-          'menu_class' => 'nav-link',
+          'theme_location' => 'beautiful-theme-top-menu',
+          'menu_class' => 'beaut_theme_top_menu',
         )
       );?>
     </nav>
